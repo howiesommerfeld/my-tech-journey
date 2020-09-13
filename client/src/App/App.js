2348d317a915c6e918efb4e0db24d1fa18b1fbc5
howiesommerfeld <howiesommerfeld@gmail.com>
2020-09-13 13:41:32 +0200

    Create a custom Navbar component & import it into the App
    TODO: Remove react-router-bootstrap since using native reat-dom-router Link instead

--- client/src/App/App.js
+++ client/src/App/App.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home';
 import List from './pages/List';
+import MyNavbar from './Components/MyNavbar';
 
 function App() {
   return (
     <div className="App">
       <div>
+        <MyNavbar />
         <Switch>
           <Route exact path='/' component={Home}/>
           <Route path='/list' component={List}/>
