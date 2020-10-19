566c6fcd6907e273609610231004f8458bd45c29
howiesommerfeld <howiesommerfeld@gmail.com>
2020-10-19 15:07:19 +0200

    Call API endpoint to render the experience in question from Firestore

--- client/src/App/pages/Experiences.js
+++ client/src/App/pages/Experiences.js
@@ -3,18 +3,35 @@ import {useParams} from "react-router-dom";
 
 function Experiences() {
     const [list, setList] = useState([])
+    const [busy, setBusy] = useState(false)
+    const [experience, setExperience] = useState({})
     let {experienceId} = useParams();
 
     const getList = () => {
+        setBusy(true)
         fetch('/api/getList')
         .then(res => res.json())
-        .then(list => setList(list));
+        .then(list => setList(list))
+        .catch(e=>{console.log(e.message)})
+        .finally(()=>setBusy(false));
+    }
+
+    const getExperience = () => {
+        setBusy(true)
+        fetch(`/api/experiences/${experienceId}`)
+        .then(res => res.json())
+        .then(experience => setExperience(experience))
+        .catch(e=>{console.log(e.message)})
+        .finally(()=>setBusy(false));
     }
 
     useEffect(()=>{
-        getList();
-        console.log("Experience ID", experienceId)
-    },[])
+        if(typeof experienceId !== "underfined" && experienceId !== ""){
+            getExperience();
+        } else {
+            getList();
+        }
+    },[experienceId])
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -23,23 +40,11 @@ function Experiences() {
     return (
         <div className="App">
         {experienceId === "" ? <h1>List of Experiences</h1> : <h1>List of Experiences at {capitalizeFirstLetter(experienceId)}</h1>}
-        {/* Check to see if any items are found*/}
-        {list.length ? (
-            <div>
-            {/* Render the list of items */}
-            {list.map((item) => {
-                return(
-                <div>
-                    {item}
-                </div>
-                );
-            })}
-            </div>
-        ) : (
-            <div>
-            {experienceId === "" ? <h2>No Experiences Found</h2> : <h2>No Experiences Found at {capitalizeFirstLetter(experienceId)}</h2>}
-            </div>
-        )
+        {busy ?
+        <div className="spinner-border" role="status">
+          <span className="sr-only">Loading...</span>
+        </div> :
+        experience === {} ? <div>No Experiences Found</div>:<h1>{experience.title}</h1>
         }
         </div>
     );
