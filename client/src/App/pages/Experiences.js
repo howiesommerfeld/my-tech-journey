20259a613fd16bd0d8dbf475c82a4201fc382f4b
howiesommerfeld <howiesommerfeld@gmail.com>
2020-10-26 15:02:45 +0200

    Adjust alignment of spinner to be centered

--- client/src/App/pages/Experiences.js
+++ client/src/App/pages/Experiences.js
@@ -37,15 +37,15 @@ function Experiences() {
   },[experienceId])
 
   return (
-    <div style={{textAlign:"left"}}>
+    <div>
     {busy ?
-    <div className="spinner-border" role="status">
+    <div style={{textAlign:"center"}} className="spinner-border" role="status">
       <span className="sr-only">Loading...</span>
     </div> :
     experience === null ? 
         <div>No Experiences Found</div>
         :
-        <div>
+        <div style={{textAlign:"left"}}>
           <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
           <div class="col-md-8 px-0">
               <h1 class="display-4">{capitalizeFirstLetter(experienceId)}</h1>
