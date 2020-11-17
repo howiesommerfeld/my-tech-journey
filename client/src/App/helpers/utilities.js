e8575b099ee7b3bc0196b6cb00d6df13844e1f5a
howiesommerfeld <howiesommerfeld@gmail.com>
2020-11-17 08:14:06 +0200

    Add method to set title & meta description dynamically on each page

--- client/src/App/helpers/utilities.js
+++ client/src/App/helpers/utilities.js
@@ -1,3 +1,11 @@
+export function seo(data = {}) {
+  data.title = data.title || 'From App Developer Graduate to Startup Founder; This Is My Tech Journey';
+  data.metaDescription = data.metaDescription || "This is the documentation of my professional journey to date as a technical starup founder. It aims to serve as a portfolio of my career thus, a guide for aspiring entrepreneurs and a place for my learnings and experiences to live. ";
+
+  document.title = data.title;
+  document.querySelector('meta[name="description"]').setAttribute('content', data.metaDescription);
+}
+
 export function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
