d71052256e412c48107035551dab52059ee43533
howiesommerfeld <howiesommerfeld@gmail.com>
2020-11-23 12:24:41 +0200

    Use firebae admin SDK to make requests from server to Firestore
    This allows for more secure rules to be put in place on the Firestore database

--- index.js
+++ index.js
@@ -1,25 +1,19 @@
 const express = require('express');
 const path = require('path');
-const firebase = require("firebase/app");
+const admin = require('firebase-admin');
 const compression = require('compression')
 const sgMail = require('@sendgrid/mail');
 const cors = require('cors');
-require("firebase/firestore");
 require('dotenv').config()
 
-const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
-    measurementId: process.env.FIREBASE_MEASUREMENT_ID,
-  };
-  
-  // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
+});
 
 const app = express();
 app.use(compression())
@@ -55,7 +49,7 @@ app.get('/api/getList', (req,res) => {
 
 app.get('/api/:collectionId/:documentId', (req,res) => {
     let data = {};
-    const documentRef = firebase.firestore().collection(req.params.collectionId).doc(req.params.documentId);
+    const documentRef = admin.firestore().collection(req.params.collectionId).doc(req.params.documentId);
     documentRef.get()
     .then( snapshot => {
         if(snapshot.exists){
@@ -69,7 +63,7 @@ app.get('/api/:collectionId/:documentId', (req,res) => {
 
 app.get('/api/:collectionId', (req,res) => {
     let data = [];
-    const collectionRef = firebase.firestore().collection(req.params.collectionId);
+    const collectionRef = admin.firestore().collection(req.params.collectionId);
     collectionRef.get()
     .then( querySnapshot => {
        querySnapshot.docs.forEach(doc => {
